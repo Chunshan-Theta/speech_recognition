842dacb12ffcea1a6e56a9f43c65517fc4cf9dd3
gavin.wang <gavin19950511@gmail.com>
2016-08-23 11:50:13 +0800

    Signed-off-by: gavin.wang <gavin19950511@gmail.com>

---
+++ speech_recognition/examples/audio_transcribe.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import speech_recognition as sr
+
+# obtain path to "english.wav" in the same folder as this script
+from os import path
+AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "ex1.wav")
+#AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "french.aiff")
+#AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "chinese.flac")
+
+# use the audio file as the audio source
+r = sr.Recognizer()
+with sr.AudioFile(AUDIO_FILE) as source:
+    audio = r.record(source) # read the entire audio file
+
+# recognize speech using Sphinx
+try:
+    print("Sphinx(offline) : " + r.recognize_sphinx(audio))
+except sr.UnknownValueError:
+    print("Sphinx could not understand audio")
+except sr.RequestError as e:
+    print("Sphinx error; {0}".format(e))
+
+
+# recognize speech using Houndify
+HOUNDIFY_CLIENT_ID = "97SVPMnz1w7T0wzKB8L79w==" # Houndify client IDs are Base64-encoded strings
+HOUNDIFY_CLIENT_KEY = "cFtuYqMdyK5yRz17kQQi5iqeQ9wTc9MUwXLei5abPTbLMrl2ZLkcBCFFi9hOc-6Fwtx6dR2tc4BhkT5AYBh_mw==" # Houndify client keys are Base64-encoded strings
+try:
+    print("Houndify : " + r.recognize_houndify(audio, client_id=HOUNDIFY_CLIENT_ID, client_key=HOUNDIFY_CLIENT_KEY))
+except sr.UnknownValueError:
+    print("Houndify could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Houndify service; {0}".format(e))
+
+# recognize speech using api.ai
+API_AI_CLIENT_ACCESS_TOKEN = "851d755279ef40fcb7394fd5e058fe9b" # api.ai keys are 32-character lowercase hexadecimal strings
+try:
+    print("api.ai : " + r.recognize_api(audio, client_access_token=API_AI_CLIENT_ACCESS_TOKEN))
+except sr.UnknownValueError:
+    print("api.ai could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from api.ai service; {0}".format(e))
+
+
+# recognize speech using Wit.ai
+WIT_AI_KEY = "G4R357JHVZI2B62KG7VFVZAKJPMHVBWW" # Wit.ai keys are 32-character uppercase alphanumeric strings
+try:
+    print("Wit.ai(Free): " + r.recognize_wit(audio, key=WIT_AI_KEY))
+except sr.UnknownValueError:
+    print("Wit.ai could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Wit.ai service; {0}".format(e))
+
+
+# recognize speech using IBM Speech to Text
+IBM_USERNAME = "913b7804-d0d0-4dc5-8b42-32da7e5e3046" # IBM Speech to Text usernames are strings of the form XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
+IBM_PASSWORD = "Upi2DwkrWPQR" # IBM Speech to Text passwords are mixed-case alphanumeric strings
+try:
+    print("IBM :" + r.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD))
+except sr.UnknownValueError:
+    print("IBM Speech to Text could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from IBM Speech to Text service; {0}".format(e))
